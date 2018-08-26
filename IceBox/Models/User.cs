f6456bf44fdb3793310faef5bf8093138295c077
Jason <jasonccmfry@gmail.com>
2018-08-26 00:34:08 -0700

    Added basic models

---
+++ IceBox/Models/User.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IceBox.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public DateTime LastLogin { get; set; }
+
+        [ForeignKey("Accont")]
+        public string GUID { get; set; }
+        [ForeignKey("UserStatus")]
+        public int UserStatusId { get; set; }
+
+        public IdentityUser Account { get; set; }
+        public UserStatus UserStatus { get; set; }
+    }
+}
---
+++ IceBox/Models/UserStatus.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IceBox.Models
+{
+    public class UserStatus
+    {
+        public int UserStatusId { get; set; }
+        public string Name { get; set; }
+    }
+}
